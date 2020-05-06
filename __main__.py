b32bace67d5763f36b1de5d17e80dde5139821b3
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-05-06 20:52:21 +0200

    [FEATURE] measure elapsed time

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from string import letters, digits, punctuation
 from bruteforce import check
+import time
 
 # use all letters, digits, and special characters for brute forcing
 characters = letters + digits + punctuation
@@ -8,6 +9,9 @@ token = str(raw_input("Insert token: "))
 
 maxlength = int(raw_input("Max length of secret: "))
 
-secret = check(token, characters, maxlength) or ""
+start = time.time()
+secret = check(token, characters, maxlength+1)
+end = time.time()
+print "Elapsed time: " + str(end - start) + "s"
 
 print "The secret used to generate the token is \""+ secret +"\"."
