b9eea0b1d8da7d531a56325c2699841fd563be20
Daniel Kaufmann <daniel.kaufmann@hebotek.at>
2020-05-06 18:02:03 +0200

    [FEATURE] add main file

---
+++ __main__.py
@@ -0,0 +1,13 @@
+from string import letters, digits, punctuation
+from bruteforce import check
+
+# use all letters, digits, and special characters for brute forcing
+characters = letters + digits + punctuation
+
+token = str(raw_input("Insert token: "))
+
+maxlength = int(raw_input("Max length of secret: "))
+
+secret = check(token, characters, maxlength)
+
+print "The secret used to generate the token is \""+ secret +"\"."
