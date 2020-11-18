61663f59022e2c780b0f2c07ec362a6c46170e7a
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-11-18 18:18:41 +0100

    [BUGFIX] properly shut down pool

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,5 @@
 import itertools
-from multiprocessing import Pool, Event, cpu_count
+from multiprocessing import Pool, cpu_count
 from multiprocessing.sharedctypes import Value, Array
 from ctypes import c_char_p
 from signature_validation import validate
@@ -10,37 +10,33 @@ import time
 sharedToken = None
 sharedN = None
 sharedC = None
-event = None
 
 def checkSecret(secret):
     global sharedToken
     global sharedN
     global sharedC
-
-    if(event.is_set()):
-        return
+    
     with sharedC.get_lock():
             sharedC.value += 1
     secret = "".join(secret)
     print("N="+ str(sharedN.value) + " - tried secret: \"" + secret + "\"", end='\r')
     if validate(sharedToken.value, secret):
-        event.set()
-        outputSecret(secret)
+        return secret
 
 def check(token, characters, maxlength):
     global sharedToken
     global sharedN
     global sharedC
-    global event
 
     sharedToken = Value(c_char_p, token.encode())
     sharedN = Value('i', 0)
     sharedC = Value('i', 0)
-    event = Event()
 
     cores = cpu_count()
 
     pool = Pool(cores)
+
+    secretFound = False
 
     print("Number of cores in use: " + str(cores))
     
@@ -48,15 +44,22 @@ def check(token, characters, maxlength):
         with sharedN.get_lock():
             sharedN.value = n
         generator = itertools.product(characters, repeat = n)
-        pool.imap_unordered(checkSecret, generator, 5000)
-    pool.close()
-    pool.join()
-    outputSecret(None)
+        for secret in pool.imap_unordered(checkSecret, generator, 5000):
+            if secret:
+                shutDownPool(pool)
+                secretFound = True
+                outputSecret(secret)
+    if(secretFound == False):
+        shutDownPool(pool)
+        outputSecret(secret)
 def outputSecret(secret):
     global sharedC
     if(secret):
-        
         print("The secret used to generate the token is \""+ secret +"\".")
     else:
         print("Tries: " + str(sharedC.value))
         print("The secret could not be determined. Try increasing the maximum length.")
+
+def shutDownPool(pool: Pool):
+    pool.close()
+    pool.join()
